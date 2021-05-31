f49a1fafcec79f55e54779e7634ee92cc66ad287
justynadudar <62484042+justynadudar@users.noreply.github.com>
2021-05-31 07:37:43 +0200

    Adding employees to the table

--- src/main/java/notApplication/Main.java
+++ src/main/java/notApplication/Main.java
@@ -1,40 +1,62 @@
 package notApplication;
 
-import net.bytebuddy.implementation.bind.MethodDelegationBinder;
-
-import java.time.Clock;
-import java.util.Scanner;
-
 public class Main {
+    static int[][] idPracownika = new int[31][2];
+
+    public Main(){
+        for(int i=1; i<=30; i++){
+            idPracownika[i][0]= 0;
+            idPracownika[i][1]= 0;
+        }
+    }
     public static void main(String[] args) {
-        Pracownik pracownik1 = new Pracownik();
-        //pracownik1.wpiszDostepnosc();
+        Pracownik pracownik1 = new Pracownik(1);
+        Pracownik pracownik2 = new Pracownik(2);
+        System.out.println("id1: "+pracownik1.idPracownika);
+        System.out.println("id2: "+pracownik2.idPracownika);
+        pracownik1.wpiszDostepnosc();
+        pracownik2.wpiszDostepnosc();
+
+        //tutaj powinna być strategia przeszukiwań
+        for(int i=1; i<=30; i++){
+            if(pracownik1.tabDostepnosci[i]==1){
+                idPracownika[i][0]= pracownik1.idPracownika;
+            }
+            else if(pracownik2.tabDostepnosci[i]==1){
+                idPracownika[i][0]= pracownik2.idPracownika;
+            }
+            else{
+                idPracownika[i][0] = -1;
+            }
+            //jeszcze nie wiem czy grafik potrzebny czy robimy wszystko na zasadzie tabeli dwuwymiarowej
+            //GrafikNaDzien grafik = new GrafikNaDzien(i,idPracownika[i][0], idPracownika[i][1] );
+        }
 
         //wyswietlanie tabelki
-            //sout +TAB
+            //sout +TAB  -skrot do system out
             System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
             System.out.println("|                        |           1            |           2            |           3            |           4            |           5            |           6            |");
-            System.out.println("|                        |                        |                        |                        |                        |                        |                        |");
-            System.out.println("|                        |                        |                        |                        |                        |                        |                        |");
+            System.out.println("|                        |  Pracownik "+idPracownika[1][0]+"           |  Pracownik "+idPracownika[2][0]+"           |  Pracownik "+idPracownika[3][0]+"           |  Pracownik "+idPracownika[4][0]+"           |  Pracownik "+idPracownika[5][0]+"           |                        |");
+            System.out.println("|                        |  Pracownik "+idPracownika[1][1]+"           |  Pracownik "+idPracownika[2][1]+"           |  Pracownik "+idPracownika[3][1]+"           |  Pracownik "+idPracownika[4][1]+"           |  Pracownik "+idPracownika[5][1]+"           |                        |");
             System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
             System.out.println("|           7            |           8            |           9            |           10           |           11           |           12           |           13           |");
-            System.out.println("|                        |                        |                        |                        |                        |                        |                        |");
-            System.out.println("|                        |                        |                        |                        |                        |                        |                        |");
+            System.out.println("|  Pracownik "+idPracownika[7][0]+"           |  Pracownik "+idPracownika[8][0]+"           |  Pracownik "+idPracownika[9][0]+"           |  Pracownik "+idPracownika[10][0]+"           |  Pracownik "+idPracownika[11][0]+"           |  Pracownik "+idPracownika[12][0]+"           |                        |");
+            System.out.println("|  Pracownik "+idPracownika[7][1]+"           |  Pracownik "+idPracownika[8][1]+"           |  Pracownik "+idPracownika[9][1]+"           |  Pracownik "+idPracownika[10][1]+"           |  Pracownik "+idPracownika[11][1]+"           |  Pracownik "+idPracownika[12][1]+"           |                        |");
             System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
             System.out.println("|           14           |           15           |           16           |           17           |           18           |           19           |           20           |");
-            System.out.println("|                        |                        |                        |                        |                        |                        |                        |");
-            System.out.println("|                        |                        |                        |                        |                        |                        |                        |");
+            System.out.println("|  Pracownik "+idPracownika[14][0]+"           |  Pracownik "+idPracownika[15][0]+"           |  Pracownik "+idPracownika[16][0]+"           |  Pracownik "+idPracownika[17][0]+"           |  Pracownik "+idPracownika[18][0]+"           |  Pracownik "+idPracownika[19][0]+"           |                        |");
+            System.out.println("|  Pracownik "+idPracownika[14][1]+"           |  Pracownik "+idPracownika[15][1]+"           |  Pracownik "+idPracownika[16][1]+"           |  Pracownik "+idPracownika[17][1]+"           |  Pracownik "+idPracownika[18][1]+"           |  Pracownik "+idPracownika[19][1]+"           |                        |");
             System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
             System.out.println("|           21           |           22           |           23           |           24           |           25           |           26           |           27           |");
-            System.out.println("|                        |                        |                        |                        |                        |                        |                        |");
-            System.out.println("|                        |                        |                        |                        |                        |                        |                        |");
+            System.out.println("|  Pracownik "+idPracownika[21][0]+"           |  Pracownik "+idPracownika[22][0]+"           |  Pracownik "+idPracownika[23][0]+"           |  Pracownik "+idPracownika[24][0]+"           |  Pracownik "+idPracownika[25][0]+"           |  Pracownik "+idPracownika[26][0]+"           |                        |");
+            System.out.println("|  Pracownik "+idPracownika[21][1]+"           |  Pracownik "+idPracownika[22][1]+"           |  Pracownik "+idPracownika[23][1]+"           |  Pracownik "+idPracownika[24][1]+"           |  Pracownik "+idPracownika[25][1]+"           |  Pracownik "+idPracownika[26][1]+"           |                        |");
             System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
             System.out.println("|           28           |           29           |           30           |                        |                        |                        |                        |");
-            System.out.println("|                        |                        |                        |                        |                        |                        |                        |");
-            System.out.println("|                        |                        |                        |                        |                        |                        |                        |");
+            System.out.println("|  Pracownik "+idPracownika[28][0]+"           |  Pracownik "+idPracownika[29][0]+"           |  Pracownik "+idPracownika[30][0]+"           |                        |                        |                        |                        |");
+            System.out.println("|  Pracownik "+idPracownika[28][1]+"           |  Pracownik "+idPracownika[29][1]+"           |  Pracownik "+idPracownika[30][1]+"           |                        |                        |                        |                        |");
             System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
 
-        
+
 
     }
 
--- src/main/java/notApplication/Pracownik.java
+++ src/main/java/notApplication/Pracownik.java
@@ -5,14 +5,19 @@ public class Pracownik {
     int idPracownika;
     int tabDostepnosci[] = new int[31];
 
+    public Pracownik(int id){
+        this.idPracownika = id;
+    }
+
     void wpiszDostepnosc(){
         Scanner in = new Scanner( System.in );
         System.out.println("Wpisz swoją dostępność w danym dniu (1-dostepny, 0-nie): ");
         int dostepnosc;
 
-        for(int i=1; i<=31; i++){
-            if((i+5)%7==0){
+        for(int i=1; i<=30; i++){
+            if((i+1)%7==0){
                 System.out.println("Dzien: "+ i + " jest niedzielą, tego dnia nie pracujemy ");
+                tabDostepnosci[i] = -1;
             }else{
                 System.out.println(i + ": ");
                 dostepnosc = in.nextInt();
