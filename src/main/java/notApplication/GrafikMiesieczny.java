65d944102baaea082cfe18b89ae740fa7e7f0830
olssssi <64044694+olssssi@users.noreply.github.com>
2021-06-07 17:31:53 +0200

    Viewing improved

--- src/main/java/notApplication/GrafikMiesieczny.java
+++ src/main/java/notApplication/GrafikMiesieczny.java
@@ -3,23 +3,23 @@ package notApplication;
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.Comparator;
+import java.util.concurrent.TimeUnit;
 
-public class GrafikMiesieczny  implements Comparator<GrafikMiesieczny>{
+public class GrafikMiesieczny implements Comparator<GrafikMiesieczny> {
     ArrayList<GrafikDzienny> grafik;
     int ocena;
     ArrayList<Integer> iloscHPracownikow;
 
     public GrafikMiesieczny() {
         grafik = new ArrayList<GrafikDzienny>(31);
-        ocena=0;
+        ocena = 0;
         grafik.add(null);
         iloscHPracownikow = new ArrayList<Integer>(4);//wpisalam na razie 4 nie wiem czy to nie powinno być zmienne
-
     }
 
-    public void uzupelnijGrafik (Pracownik[] pracownicy){
-        int rand1=0, rand2=0, m=-1, n=-1;
-        for(int i=1; i<=30; i++) {
+    public void uzupelnijGrafik(Pracownik[] pracownicy) {
+        int rand1 = 0, rand2 = 0, m = -1, n = -1;
+        for (int i = 1; i <= 30; i++) {
             // Te m wprowadziłam po to, żeby pętla nie wykonywała się w nieskończoność. 10 razy szuka randomowej osoby i sprawdza,
             // Czy ma dyspozycyjność danego dnia. Jeżeli 10 razy nie trafi na taką osobę, to później wprowadza null
             // Czyli, że na tą ranną zmianę nie ma komu pracować. Tak samo robię z n i wieczorną zmianą
@@ -32,16 +32,18 @@ public class GrafikMiesieczny  implements Comparator<GrafikMiesieczny>{
                 }
             }
 
-            if((i+2)%7==0){ n=1;}
-            else{
+            if ((i + 2) % 7 == 0) {
+                n = 1;
+            } else {
                 for (n = 0; n < 10; n++) {
                     rand2 = ThreadLocalRandom.current().nextInt(1, 5);
-                    if (pracownicy[rand2].tabDostepnosci[i] == 1) {
+                    if ((pracownicy[rand1] != pracownicy[rand2]) && pracownicy[rand2].tabDostepnosci[i] == 1) {
 //                        System.out.println("Znaleziono osobe na wieczorna zmiane w dniu nr " + i + "\n");
                         n = -1;
                         break;
                     }
-                }}
+                }
+            }
 
             // Znaleziono osobę na ranną i wieczorną zmianę
             if (m == -1 && n == -1) {
@@ -74,8 +76,8 @@ public class GrafikMiesieczny  implements Comparator<GrafikMiesieczny>{
         zliczIloscH(pracownicy);
     }
 
-    public void losowoZmienGrafikDzienny(int numerDnia, Pracownik[] pracownicy){
-        int rand1=0, rand2=0, m=-1, n=-1;
+    public void losowoZmienGrafikDzienny(int numerDnia, Pracownik[] pracownicy) {
+        int rand1 = 0, rand2 = 0, m = -1, n = -1;
         GrafikDzienny grafikDzienny = grafik.get(numerDnia);
         for (m = 0; m < 10; m++) {
             rand1 = ThreadLocalRandom.current().nextInt(1, 5);
@@ -85,8 +87,9 @@ public class GrafikMiesieczny  implements Comparator<GrafikMiesieczny>{
                 break;
             }
         }
-        if((numerDnia+2)%7==0){ n=1;}
-        else{
+        if ((numerDnia + 2) % 7 == 0) {
+            n = 1;
+        } else {
             for (n = 0; n < 10; n++) {
                 rand2 = ThreadLocalRandom.current().nextInt(1, 5);
                 if (pracownicy[rand2].tabDostepnosci[numerDnia] == 1) {
@@ -94,7 +97,8 @@ public class GrafikMiesieczny  implements Comparator<GrafikMiesieczny>{
                     n = -1;
                     break;
                 }
-            }}
+            }
+        }
         // Znaleziono osobę na ranną i wieczorną zmianę
         if (m == -1 && n == -1) {
 //            GrafikDzienny grafikDzienny = new GrafikDzienny(numerDnia, "poniedzialek", pracownicy[rand1], pracownicy[rand2]);
@@ -126,79 +130,86 @@ public class GrafikMiesieczny  implements Comparator<GrafikMiesieczny>{
         }
     }
 
-    public void wypisz(){
+    public void wypisz() throws InterruptedException {
         //wyswietlanie tabelki
         //sout +TAB  -skrot do system out
-        System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
+        System.out.println("|__________PON___________|___________WT___________|___________ŚR___________|__________CZW___________|__________PT____________|__________SB____________|__________NDZ___________|");
+        System.out.println("|________________________|________________________|________________________|________________________|________________________|________________________|________________________|");
         System.out.println("|                        |           1            |           2            |           3            |           4            |           5            |           6            |");
-        System.out.println("|                        |"+grafik.get(1).porannaZmiana+"                    |"+grafik.get(2).porannaZmiana+"                    |"+grafik.get(3).porannaZmiana+"                    |"+grafik.get(4).porannaZmiana+"                    |"+grafik.get(5).porannaZmiana+"                    |"+grafik.get(6).porannaZmiana+"                    |");
-        System.out.println("|                        |"+grafik.get(1).wieczornaZmiana+"                    |"+grafik.get(2).wieczornaZmiana+"                    |"+grafik.get(3).wieczornaZmiana+"                    |"+grafik.get(4).wieczornaZmiana+"                    |"+grafik.get(5).wieczornaZmiana+"                    |"+grafik.get(6).wieczornaZmiana+"                    |");
+        System.out.println("|                        |" + grafik.get(1).porannaZmiana + "                    |" + grafik.get(2).porannaZmiana + "                    |" + grafik.get(3).porannaZmiana + "                    |" + grafik.get(4).porannaZmiana + "                    |" + grafik.get(5).porannaZmiana + "                    |" + grafik.get(6).porannaZmiana + "                    |");
+        System.out.println("|                        |" + grafik.get(1).wieczornaZmiana + "                    |" + grafik.get(2).wieczornaZmiana + "                    |" + grafik.get(3).wieczornaZmiana + "                    |" + grafik.get(4).wieczornaZmiana + "                    |" + grafik.get(5).wieczornaZmiana + "                    |" + grafik.get(6).wieczornaZmiana + "                    |");
         System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
         System.out.println("|           7            |           8            |           9            |           10           |           11           |           12           |           13           |");
-        System.out.println("|"+grafik.get(7).porannaZmiana+"                    |"+grafik.get(8).porannaZmiana+"                    |"+grafik.get(9).porannaZmiana+"                    |"+grafik.get(10).porannaZmiana+"                    |"+grafik.get(11).porannaZmiana+"                    |"+grafik.get(12).porannaZmiana+"                    |"+grafik.get(13).porannaZmiana+"                    |");
-        System.out.println("|"+grafik.get(7).wieczornaZmiana+"                    |"+grafik.get(8).wieczornaZmiana+"                    |"+grafik.get(9).wieczornaZmiana+"                    |"+grafik.get(10).wieczornaZmiana+"                    |"+grafik.get(11).wieczornaZmiana+"                    |"+grafik.get(12).wieczornaZmiana+"                    |"+grafik.get(13).wieczornaZmiana+"                    |");
+        System.out.println("|" + grafik.get(7).porannaZmiana + "                    |" + grafik.get(8).porannaZmiana + "                    |" + grafik.get(9).porannaZmiana + "                    |" + grafik.get(10).porannaZmiana + "                    |" + grafik.get(11).porannaZmiana + "                    |" + grafik.get(12).porannaZmiana + "                    |" + grafik.get(13).porannaZmiana + "                    |");
+        System.out.println("|" + grafik.get(7).wieczornaZmiana + "                    |" + grafik.get(8).wieczornaZmiana + "                    |" + grafik.get(9).wieczornaZmiana + "                    |" + grafik.get(10).wieczornaZmiana + "                    |" + grafik.get(11).wieczornaZmiana + "                    |" + grafik.get(12).wieczornaZmiana + "                    |" + grafik.get(13).wieczornaZmiana + "                    |");
         System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
         System.out.println("|           14           |           15           |           16           |           17           |           18           |           19           |           20           |");
-        System.out.println("|"+grafik.get(14).porannaZmiana+"                    |"+grafik.get(15).porannaZmiana+"                    |"+grafik.get(16).porannaZmiana+"                    |"+grafik.get(17).porannaZmiana+"                    |"+grafik.get(18).porannaZmiana+"                    |"+grafik.get(19).porannaZmiana+"                    |"+grafik.get(20).porannaZmiana+"                    |");
-        System.out.println("|"+grafik.get(14).wieczornaZmiana+"                    |"+grafik.get(15).wieczornaZmiana+"                    |"+grafik.get(16).wieczornaZmiana+"                    |"+grafik.get(17).wieczornaZmiana+"                    |"+grafik.get(18).wieczornaZmiana+"                    |"+grafik.get(19).wieczornaZmiana+"                    |"+grafik.get(20).wieczornaZmiana+"                    |");
+        System.out.println("|" + grafik.get(14).porannaZmiana + "                    |" + grafik.get(15).porannaZmiana + "                    |" + grafik.get(16).porannaZmiana + "                    |" + grafik.get(17).porannaZmiana + "                    |" + grafik.get(18).porannaZmiana + "                    |" + grafik.get(19).porannaZmiana + "                    |" + grafik.get(20).porannaZmiana + "                    |");
+        System.out.println("|" + grafik.get(14).wieczornaZmiana + "                    |" + grafik.get(15).wieczornaZmiana + "                    |" + grafik.get(16).wieczornaZmiana + "                    |" + grafik.get(17).wieczornaZmiana + "                    |" + grafik.get(18).wieczornaZmiana + "                    |" + grafik.get(19).wieczornaZmiana + "                    |" + grafik.get(20).wieczornaZmiana + "                    |");
         System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
         System.out.println("|           21           |           22           |           23           |           24           |           25           |           26           |           27           |");
-        System.out.println("|"+grafik.get(21).porannaZmiana+"                    |"+grafik.get(22).porannaZmiana+"                    |"+grafik.get(23).porannaZmiana+"                    |"+grafik.get(24).porannaZmiana+"                    |"+grafik.get(25).porannaZmiana+"                    |"+grafik.get(26).porannaZmiana+"                    |"+grafik.get(27).porannaZmiana+"                    |");
-        System.out.println("|"+grafik.get(21).wieczornaZmiana+"                    |"+grafik.get(22).wieczornaZmiana+"                    |"+grafik.get(23).wieczornaZmiana+"                    |"+grafik.get(24).wieczornaZmiana+"                    |"+grafik.get(25).wieczornaZmiana+"                    |"+grafik.get(26).wieczornaZmiana+"                    |"+grafik.get(27).wieczornaZmiana+"                    |");
+        System.out.println("|" + grafik.get(21).porannaZmiana + "                    |" + grafik.get(22).porannaZmiana + "                    |" + grafik.get(23).porannaZmiana + "                    |" + grafik.get(24).porannaZmiana + "                    |" + grafik.get(25).porannaZmiana + "                    |" + grafik.get(26).porannaZmiana + "                    |" + grafik.get(27).porannaZmiana + "                    |");
+        System.out.println("|" + grafik.get(21).wieczornaZmiana + "                    |" + grafik.get(22).wieczornaZmiana + "                    |" + grafik.get(23).wieczornaZmiana + "                    |" + grafik.get(24).wieczornaZmiana + "                    |" + grafik.get(25).wieczornaZmiana + "                    |" + grafik.get(26).wieczornaZmiana + "                    |" + grafik.get(27).wieczornaZmiana + "                    |");
         System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
         System.out.println("|           28           |           29           |           30           |                        |                        |                        |                        |");
-        System.out.println("|"+grafik.get(28).porannaZmiana+"                    |"+grafik.get(29).porannaZmiana+"                    |"+grafik.get(30).porannaZmiana+"                    |                        |                        |                        |                        |");
-        System.out.println("|"+grafik.get(28).wieczornaZmiana+"                    |"+grafik.get(29).wieczornaZmiana+"                    |"+grafik.get(30).wieczornaZmiana+"                    |                        |                        |                        |                        |");
+        System.out.println("|" + grafik.get(28).porannaZmiana + "                    |" + grafik.get(29).porannaZmiana + "                    |" + grafik.get(30).porannaZmiana + "                    |                        |                        |                        |                        |");
+        System.out.println("|" + grafik.get(28).wieczornaZmiana + "                    |" + grafik.get(29).wieczornaZmiana + "                    |" + grafik.get(30).wieczornaZmiana + "                    |                        |                        |                        |                        |");
         System.out.println("_______________________________________________________________________________________________________________________________________________________________________________");
 
         wypiszIloscH();
 
-        if(sprawdzPoprawnosc()) System.out.println("Stworzono poprawny grafik");
-        else    System.out.println("Nie stworzono poprawnego grafiku");
+        TimeUnit.SECONDS.sleep(4);
+        System.out.println("\nPOPRAWNOŚĆ:\n");
+        TimeUnit.SECONDS.sleep(2);
+        if (sprawdzPoprawnosc()) System.out.println("Stworzono poprawny grafik");
+        else System.out.println("Nie stworzono poprawnego w całości grafiku");
 
     }
 
 
-    //TODO Trzeba jeszcze sprawdzać, czy pracownik pracuje odpowiednia ilosc h w miesiacu
-    public void zliczIloscH(Pracownik[] pracownicy){
-        for(int i=1; i<pracownicy.length; i++){
-            pracownicy[i].iloscH=0;
+    public void zliczIloscH(Pracownik[] pracownicy) {
+        for (int i = 1; i < pracownicy.length; i++) {
+            pracownicy[i].iloscH = 0;
         }
 
-            for(int j=1; j<=30; j++){
-                if(grafik.get(j).porannaZmiana!=null)
-                grafik.get(j).porannaZmiana.iloscH+=8;
-                if(grafik.get(j).wieczornaZmiana!=null)
-                grafik.get(j).wieczornaZmiana.iloscH+=8;
-            }
-        for(int j=1; j<pracownicy.length; j++){
-           iloscHPracownikow.add(pracownicy[j].iloscH);
+        for (int j = 1; j <= 30; j++) {
+            if (grafik.get(j).porannaZmiana != null)
+                grafik.get(j).porannaZmiana.iloscH += 8;
+            if (grafik.get(j).wieczornaZmiana != null)
+                grafik.get(j).wieczornaZmiana.iloscH += 8;
+        }
+        for (int j = 1; j < pracownicy.length; j++) {
+            iloscHPracownikow.add(pracownicy[j].iloscH);
         }
 
     }
 
-    public boolean wypiszIloscH(){
-
-        for(int j=0; j<iloscHPracownikow.size(); j++){
-            System.out.println("id: "+ (j+1) + " ilosc h: "+ iloscHPracownikow.get(j));
+    public boolean wypiszIloscH() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(4);
+        System.out.println("\nIlość godzin poszczególnych pracowników:\n");
+        TimeUnit.SECONDS.sleep(2);
+        System.out.println("ID PRACOWNIKA    |   ILOŚĆ GODZIN");
+        for (int j = 0; j < iloscHPracownikow.size(); j++) {
+            System.out.println("      " + (j + 1) + "          |        " + iloscHPracownikow.get(j));
         }
         return true;
     }
+
     //Specjalnie nie robiłam tego na razie, bo nasz algorytm jeszcze nie znajduje tak dobrego rozwiazania
-    public boolean sprawdzPoprawnosc(){
-        for(int i=1; i<=30; i++){
-            if(!grafik.get(i).sprawdzPoprawnosc(grafik.get(i-1))) {
-                System.out.printf("Pierwszy napotkany niepoprawny dzień: "+i+"\n");
+    public boolean sprawdzPoprawnosc() {
+        for (int i = 1; i <= 30; i++) {
+            if (!grafik.get(i).sprawdzPoprawnosc(grafik.get(i - 1))) {
+                System.out.printf("Pierwszy napotkany niepoprawny dzień: " + i + "\n");
                 return false;
             }
         }
         return true;
     }
 
-    public int zliczPoprawneDni(){
+    public int zliczPoprawneDni() {
         int iloscPoprawnychDni = 0;
-        for(int i=1; i<=30; i++){
-            if(grafik.get(i).sprawdzPoprawnosc(grafik.get(i-1))) {
+        for (int i = 1; i <= 30; i++) {
+            if (grafik.get(i).sprawdzPoprawnosc(grafik.get(i - 1))) {
                 iloscPoprawnychDni++;
             }
         }
@@ -218,11 +229,11 @@ public class GrafikMiesieczny  implements Comparator<GrafikMiesieczny>{
     @Override
     public int compare(GrafikMiesieczny o1, GrafikMiesieczny o2) {
         int ocena1, ocena2;
-        if(o1==null) ocena1 = 0;
+        if (o1 == null) ocena1 = 0;
         else ocena1 = o1.ocena;
-        if(o2==null) ocena2 = 0;
+        if (o2 == null) ocena2 = 0;
         else ocena2 = o2.ocena;
-        return  ocena2 - ocena1;
+        return ocena2 - ocena1;
     }
 
 }
--- src/main/java/notApplication/Main.java
+++ src/main/java/notApplication/Main.java
@@ -2,9 +2,11 @@ package notApplication;
 
 import java.io.FileNotFoundException;
 import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException, InterruptedException {
+        double najlepszy=0,najgorszy=0;
         Pracownik[] pracownicy = new Pracownik[5];
         Pracownik pracownik1 = new Pracownik(1);
         Pracownik pracownik2 = new Pracownik(2);
@@ -60,25 +62,71 @@ public class Main {
         populacja.grafiki[9]=grafikMiesieczny9;
         populacja.grafiki[10]=grafikMiesieczny10;
         populacja.sortuj();
-        System.out.println(populacja);
+
+        System.out.println("\n_______________________FAZA TWORZENIA POPULACJI:_______________________________________");
+        System.out.println("W sposób losowy zostaje stworzonych 11 grafików.");
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("\n. . .\n");
+        TimeUnit.SECONDS.sleep(3);
+        System.out.println("_______________________WYNIKI PO LOSOWANIU:____________________________________________");
+        System.out.println("Poprawność aktualnie najlepszego grafiku: "+populacja.grafiki[0].ocena+"/30");
+        System.out.println("Poprawność aktualnie najgorszego grafiku: "+populacja.grafiki[10].ocena+"/30\n\n\n");
+        TimeUnit.SECONDS.sleep(4);
+
+
 
         // Selekcja
+        System.out.println("\n\n_______________________FAZA SELEKCJI___________________________________________________");
+        System.out.println("Rozpoczynana jest selekcja. Zostanie przeprowadzonych 100 iteracji. " +
+                            "\nW każdej z nich zostanie odrzucone 20% najsłabszych grafików miesięcznych." +
+                            "\nNa ich miejsce zostaną wylosowane nowe.");
         populacja.dokonajSelekcji(100,20,pracownicy);
-        System.out.println("\nPO SELEKCJI:\n");
-        System.out.println(populacja);
+        TimeUnit.SECONDS.sleep(6);
+        System.out.println("\n. . .\n");
+        TimeUnit.SECONDS.sleep(6);
+        System.out.println("_______________________WYNIKI PO SELEKCJI:_____________________________________________");
+        System.out.println("Poprawność aktualnie najlepszego grafiku: "+populacja.grafiki[0].ocena+"/30");
+        System.out.println("Poprawność aktualnie najgorszego grafiku: "+populacja.grafiki[10].ocena+"/30\n\n\n");
+        TimeUnit.SECONDS.sleep(4);
+
+
 
         // Krzyżowanie
+        System.out.println("\n\n_______________________FAZA KRZYŻOWANIA________________________________________________");
+        System.out.println("Rozpoczynana jest faza krzyżowania. 60% grafików miesięcznych zostanie skrzyżowanych między sobą." +
+                            "\nOznacza to, że 50% dni każdego z nich zostanie wymienione na tą samą ilość dni z innego krzyżowanego miesiąca.");
         populacja.krzyzuj(60,50,pracownicy);
         populacja.sortuj();
-        System.out.println("\nPO KRZYZOWANIU:\n");
-        System.out.println(populacja);
+        TimeUnit.SECONDS.sleep(6);
+        System.out.println("\n. . .\n");
+        TimeUnit.SECONDS.sleep(6);
+        System.out.println("_______________________WYNIKI PO KRZYŻOWANIU:__________________________________________");
+        System.out.println("Poprawność aktualnie najlepszego grafiku: "+populacja.grafiki[0].ocena+"/30");
+        System.out.println("Poprawność aktualnie najgorszego grafiku: "+populacja.grafiki[10].ocena+"/30\n\n\n");
+        TimeUnit.SECONDS.sleep(4);
+
+
 
         // Mutacja
+        System.out.println("\n\n_______________________FAZA MUTACJI____________________________________________________");
+        System.out.println("Rozpoczynana jest faza mutacji. 20% grafików miesięcznych zostanie zmutowanych." +
+                            "\nOznacza to, że 20% dni każdego z grafików zostanie na nowo wylosowane.");
         populacja.mutuj(20,20,pracownicy);
-        System.out.println("\nPO MUTACJI:\n");
-        System.out.println(populacja);
+        TimeUnit.SECONDS.sleep(6);
+        System.out.println("\n. . .\n");
+        TimeUnit.SECONDS.sleep(6);
+        System.out.println("_______________________WYNIKI PO MUTACJI:______________________________________________");
+        System.out.println("Poprawność aktualnie najlepszego grafiku: "+populacja.grafiki[0].ocena+"/30");
+        System.out.println("Poprawność aktualnie najgorszego grafiku: "+populacja.grafiki[10].ocena+"/30\n\n\n");
+        TimeUnit.SECONDS.sleep(4);
+
 
         //wyswietlanie najlepszego grafiku
+
+        System.out.println("_______________________________________________________________________________________");
+        System.out.println("_______________________RAPORT KOŃCOWY:_________________________________________________");
+        System.out.println("\n\nWidok najlepszego grafiku:\n");
+        TimeUnit.SECONDS.sleep(3);
         populacja.grafiki[0].wypisz();
 
     }
